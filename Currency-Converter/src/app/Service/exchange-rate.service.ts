cdab3260b0b20907519ebcf8a0ee509be472f342
jeswanth-533 <mjeswanth33@gmail.com>
2021-03-30 15:28:37 -0400

    changes

--- Currency-Converter/src/app/Service/exchange-rate.service.ts
+++ Currency-Converter/src/app/Service/exchange-rate.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {DatePipe} from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExchangeRateService {
 latestRates:string = "https://api.exchangeratesapi.io/latest";
-  constructor(private httpClient:HttpClient) { }
+ratesForAnyDay:string = "https://api.exchangeratesapi.io/";
+ratesForYear:string = "https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-0";
+formattedDate:string;
+
+  constructor(private httpClient:HttpClient,
+    private dateFormat:DatePipe) { }
 
   public getLatestrates():Observable<any>{
     return this.httpClient.get<any>(this.latestRates);
   }
 
-  public getQuoteByDifferentCurrency():Observable<any>{
-    return 
+  //Get historical rates for any day since 1999
+  public getHistoricalRates(date:string):Observable<any>{
+    this.formattedDate = this.dateFormat.transform(date,"yyyy-MM-dd");
+    var url = this.ratesForAnyDay + this.formattedDate;
+    return  this.httpClient.get<any>(url);
   }
+
+  //Get historical rates for a time period
+  public getHistoricalRatesForYear(startDate:string,endDate:string){
+
+  }
+
 }
--- Currency-Converter/src/app/tab1/tab1.page.ts
+++ Currency-Converter/src/app/tab1/tab1.page.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {ExchangeRateService} from '../Service/exchange-rate.service';
-import {Observable,Subscriber} from 'rxjs';
+import { ExchangeRateService } from '../Service/exchange-rate.service';
+import { Observable, Subscriber } from 'rxjs';
+
+export interface rates{
+  currency:string;
+  exchangeRate:number;
+} 
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements OnInit{
-ratesList:any;
-  constructor(private exchangeRate:ExchangeRateService) {}
 
-  ngOnInit(){
-this.exchangeRate.getLatestrates().subscribe(list => {
-  debugger;
-this.ratesList = list;
-});
+export class Tab1Page implements OnInit {
+  ratesList: any;
+  amountEntered: number;
+  constructor(private exchangeRate: ExchangeRateService) {}
+
+  ngOnInit() {
+    this.exchangeRate.getLatestrates().subscribe((list) => {
+      this.ratesList = list;
+    });
+    this.callRates();
   }
 
+  callRates(event?: any, value?: number) {
+    value = value > 1 ? value:1; 
+    // this.rate.push(this.ratesList.rates);
+    console.log(this.ratesList.rates);
+  }
 }
